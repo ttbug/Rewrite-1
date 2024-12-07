1fa7dedf7367bfe838cac29130bb6e3e1b6a7cf1
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2024-12-07 10:23:29 +0800

    更新 woniushuimian.js

--- woniushuimian.js
+++ woniushuimian.js
@@ -2,6 +2,7 @@
 
 项目名称：蜗牛睡眠
 下载地址：https://t.cn/A6NBzdYh
+更新日期：2024-12-07
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -19,30 +20,19 @@ hostname = snailsleep.net
 
 var chxm1023 = JSON.parse($response.body);
 
-chxm1023 = {
-  "status" : 200,
-  "message" : "OK",
-  "result" : {
-    "points" : 0,
-    "updated" : 1684116994159,
-    "expired" : false,
-    "expiresRAD" : 4092599349000,
-    "offerPeriod" : false,
-    "expires" : 4092599349000,
-    "type" : "SNAIL",
-    "dreamsTotal" : 10,
-    "level" : 1,
-    "unique" : "646195d5bc81021ffed65e71",
-    "lifelong" : true,
-    "radexpired" : false,
-    "links" : [
-
-    ],
-    "dreamsUsed" : 0
-  },
-  "links" : [
-
-  ]
+chxm1023.result = {
+  ...chxm1023.result,
+  "points" : 9999,
+  "updated" : 1684116994159,
+  "expired" : false,
+  "expiresRAD" : 4092599349000,
+  "expires" : 4092599349000,
+  "type" : "SNAIL",
+  "dreamsTotal" : 999,
+  "level" : 1,
+  "lifelong" : true,
+  "radexpired" : false,
+  "dreamsUsed" : 999
 };
 
 $done({body : JSON.stringify(chxm1023)});
